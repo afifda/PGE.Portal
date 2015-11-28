a677db94b9b6c62b5089f0d77e1cdd1100806eab
afifda <afif.azh@gmail.com>
2015-11-28 13:55:44 +0700

    add ashx

---
+++ PGE.Portal/Layouts/GenericPortal.ashx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+using System.Web;
+using System.Runtime.InteropServices;
+
+namespace PGE.Portal.Layouts
+{
+    [Guid("1aa2f88c-84c8-42f9-b349-451ceaf205df")]
+    public partial class GenericPortal : IHttpHandler
+    {        
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string method = context.Request.Params["Method"];
+            switch (method.ToLower())
+            {           
+                default:
+                    break;
+            }
+        }
+    }
+}
